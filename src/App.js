1dcbdbe536ff64d55559e90e1ae9ebce6e3ce942
Joanna <morningstarjk@hotmail.com>
2017-10-18 14:49:24 -0400

    Modified App.js and added clock.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Clock from './clock';
 import './App.css';
 
 class App extends Component {
@@ -8,12 +8,10 @@ class App extends Component {
     this.state = {
       deadline: 'December 25, 2017',
       newDeadline: ''
-
     }
   }
 
   changeDeadLine() {
-
     this.setState({deadline: this.state.newDeadline});
   }
 
@@ -21,15 +19,18 @@ class App extends Component {
     return (
       <div className="App">
         <h3 className="App-title">Countdown to {this.state.deadline}</h3>
-        <div className="Clock-days">14 days</div>
-        <div className="Clock-hours">30 hours</div>
-        <div className="Clock-minutes">15 minutes</div>
-        <div className="Clock-seconds">20 seconds</div>
+
+        <div>
+          <Clock
+            deadline={this.state.deadline}
+          />
+
+        </div>
 
         <div className="App-input">
-          <input className="App-input-style" placeholder='new date' onChange={event => this.setState({newDeadline: event.target.value})}/>
+          <input className="App-input-style" placeholder='New Date' onChange={event => this.setState({newDeadline: event.target.value})}/>
           <br/>
-          <button onClick={() => this.changeDeadLine()}>Submint</button>
+          <button onClick={() => this.changeDeadLine()}>Submit</button>
         </div>
 
       </div>
